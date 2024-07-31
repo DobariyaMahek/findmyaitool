67afa0e32a2c11a0975257079e8d1577a2ae800a
mahek <mahek282.rejoice@gmail.com>
2024-07-31 17:38:38 +0530

    error file change

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic";
 const NoDataFound = dynamic(() => import("../shared/components/404"), {
   ssr: false,
 });
-export const runtime = "experimental-edge";
+export const runtime = "edge";
 
 const Custom404 = () => {
   return <NoDataFound />;
