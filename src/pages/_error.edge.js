73caf76cf747097b66381974dc28bdec16acf1b7
mahek <mahek282.rejoice@gmail.com>
2024-08-01 10:59:19 +0530

    create error file

---
+++ src/pages/_error.edge.js
@@ -0,0 +1,10 @@
+import NoDataFound from "@/shared/components/404";
+import React from "react";
+
+export const runtime = "edge";
+
+const _error = () => {
+  return <NoDataFound />;
+};
+
+export default _error;
