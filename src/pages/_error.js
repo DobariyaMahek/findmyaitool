b69239e3de57a908a4d44ab12f72f4dd3b086697
mahek <mahek282.rejoice@gmail.com>
2024-08-01 10:52:01 +0530

    error fix

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,6 +1,11 @@
 import NoDataFound from "@/shared/components/404";
 import React from "react";
-export const runtime = "edge";
+
+export const runtime =
+  process.env.NEXT_PUBLIC_USE_EDGE_RUNTIME === "true"
+    ? "edge"
+    : "experimental-edge";
+
 const _error = () => {
   return <NoDataFound />;
 };
