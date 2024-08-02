9d4af888939db4dbf8a64bca31c4658331a96610
mahek <mahek282.rejoice@gmail.com>
2024-08-02 11:47:09 +0530

    add edge

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,7 +1,7 @@
 import NoDataFound from "@/shared/components/404";
 import React from "react";
 
-export const runtime = "experimental-edge";
+export const runtime = "edge";
 
 const _error = () => {
   return <NoDataFound />;
